5a634bc3f8d4ea8262cc29c342a0c933463ebb3b
Aaron Silinskas <aaron@mindwidgets.com>
2020-12-09 22:49:19 -0800

    forgot the team parameter in spell event

--- tool/event.py
+++ tool/event.py
@@ -7,11 +7,11 @@ class Events(object):
     def __init__(self, out):
         self._out = out
 
-    def send_spell(self, name, power):
+    def send_spell(self, name, power, team=1):
         spell_id = SPELL_IDS[name]
         power_byte = 255 * power & 0xFF
 
-        print("Sending event: ", name, spell_id, power_byte)
+        print("Sending event: ", name, spell_id, power_byte, team)
 
-        data = [SPELL_EVENT, spell_id, power_byte]
+        data = [SPELL_EVENT, spell_id, power_byte, team]
         self._out.send(data)
