5c8a6bacda8a3a6f5fa557589283e1e9d693bd93
aaronsilinskas <aaron@mindwidgets.com>
2023-04-20 22:19:47 -0400

    Tool2 hardware test code

---
+++ tool2/hardware-test/code.py
@@ -0,0 +1,110 @@
+import time
+from digitalio import DigitalInOut, Direction, Pull
+import pulseio
+import busio
+import board
+import neopixel
+import adafruit_rfm69
+import adafruit_lis3dh
+import adafruit_drv2605
+from mindwidgets_df1201s import DF1201S
+from infrared import Infrared
+
+# Pins
+PIN_NEOPIXEL = board.D5
+PIN_DFPLAYER_TX = board.TX
+PIN_DFPLAYER_RX = board.RX
+PIN_TRIGGER = board.A1
+PIN_IR_RECEIVER = board.A2
+PIN_IR_TRANSMITTER = board.A3
+
+# Set up Neopixel
+NUM_PIXELS = 12  # NeoPixel strip length (in pixels)
+strip = neopixel.NeoPixel(PIN_NEOPIXEL, NUM_PIXELS,
+                          brightness=1, auto_write=False)
+
+# Set up Radio
+RADIO_FREQ_MHZ = 915.0
+radio_chip_select = DigitalInOut(board.RFM_CS)
+radio_reset = DigitalInOut(board.RFM_RST)
+
+# Initialise RFM69 radio
+radio = adafruit_rfm69.RFM69(
+    board.SPI(), radio_chip_select, radio_reset, RADIO_FREQ_MHZ, high_power=False)
+radio.listen()
+
+# Set up I2C
+i2c = board.STEMMA_I2C()
+
+# Set up accelerometer on I2C bus, 4G range
+accel = adafruit_lis3dh.LIS3DH_I2C(i2c)
+accel.range = adafruit_lis3dh.RANGE_4_G
+accel.set_tap(1, 100)
+
+# Set up vibrator driver
+vibrator = adafruit_drv2605.DRV2605(i2c)
+
+# # Set up audio
+uart = busio.UART(tx=PIN_DFPLAYER_TX, rx=PIN_DFPLAYER_RX, baudrate=115200)
+
+df_player = DF1201S(uart)
+df_player.volume = 0.3
+df_player.disable_led()
+df_player.disable_prompt()
+df_player.enable_amp()
+df_player.play_mode = DF1201S.PLAYMODE_PLAY_ONCE
+
+# # Set up IR
+ir_pulsein = pulseio.PulseIn(pin=PIN_IR_RECEIVER, maxlen=256, idle_state=True)
+ir_pulseout = pulseio.PulseOut(
+    pin=PIN_IR_TRANSMITTER, frequency=38000, duty_cycle=2 ** 15)
+infrared = Infrared(ir_pulseout, ir_pulsein)
+
+# Set up trigger
+trigger = DigitalInOut(PIN_TRIGGER)
+trigger.direction = Direction.INPUT
+trigger.pull = Pull.UP
+
+
+###########
+
+pixel_num = 0
+pixel_color = (64, 0, 0)
+
+while True:
+    if trigger.value == 0:
+        print("Trigger")
+        pulses = b"Hello!"
+        infrared.send(pulses)
+        df_player.play_next()
+        vibrator.sequence[0] = adafruit_drv2605.Effect(83)
+        vibrator.sequence[1] = adafruit_drv2605.Effect(37)
+        vibrator.sequence[2] = adafruit_drv2605.Effect(74)
+        vibrator.play()
+
+    ir_received = infrared.receive()
+    if ir_received is not None:
+        data, strength = ir_received
+        print("IR Data Received: ", data.decode("utf-8"), strength)
+
+    if radio.payload_ready():
+        radio_received = radio.receive(keep_listening=True)
+        print("Radio Data Received: ", 
+              radio_received.decode("utf-8"), radio.last_rssi)
+
+    strip[pixel_num] = pixel_color
+    pixel_num = pixel_num + 1
+    if pixel_num >= NUM_PIXELS:
+        pixel_num = 0
+        if pixel_color == 0:
+            pixel_color = (64, 0, 0)
+        else:
+            pixel_color = 0
+    strip.show()
+
+    x, y, z = accel.acceleration
+    print(x, y, z)
+    if accel.tapped:
+        print("Tapped!")
+
+    time.sleep(0.2)
---
+++ tool2/hardware-test/infrared.py
@@ -0,0 +1,152 @@
+import array
+import time
+
+IR_UNIT = 500
+IR_ERROR_MARGIN = IR_UNIT / 2
+
+IR_HEADER_MARK = IR_UNIT * 8
+IR_HEADER_SPACE = IR_UNIT * 6
+
+IR_ZERO = IR_UNIT
+IR_ONE = IR_UNIT * 3
+IR_LEAD_OUT = IR_UNIT * 10
+
+IR_CRC_GENERATOR = 0x1D
+
+
+def _calculate_crc(data):
+    crc = 0
+    for data_byte in data:
+        crc ^= data_byte
+        for j in range(8):
+            if crc & 0x80 != 0:
+                crc = ((crc << 1) & 0xFF) ^ IR_CRC_GENERATOR
+            else:
+                crc = (crc << 1) & 0xFF
+    return crc
+
+
+class Infrared(object):
+    def __init__(self, ir_pulseout, ir_pulsein):
+        self._ir_pulseout = ir_pulseout
+        self._ir_pulsein = ir_pulsein
+        self._decoder = IRDecoder()
+
+    def send(self, data):
+        self._wait_for_traffic()
+
+        print("IR Sending: ", data)
+        # length = header + (data * 8 bits per byte) + crc bits + lead out
+        durations = array.array("H", [0] * (2 + len(data) * 8 + 8 + 1))
+        durations[0] = IR_HEADER_MARK
+        durations[1] = IR_HEADER_SPACE
+        durations[-1] = IR_LEAD_OUT
+
+        duration_index = 2
+        for data_byte in data:
+            self._encode_byte(durations, duration_index, data_byte)
+            duration_index += 8
+
+        crc = _calculate_crc(data)
+        # print("CRC: ", bin(crc))
+        self._encode_byte(durations, duration_index, crc)
+
+        # print("Durations: ", durations)
+        self._ir_pulseout.send(durations)
+        # print("Sent")
+
+    def _wait_for_traffic(self):
+        while True:
+            last_len = len(self._ir_pulsein)
+            time.sleep(IR_HEADER_MARK / 38000)
+            if last_len == len(self._ir_pulsein):
+                break
+            else:
+                print("Waiting in IR traffic ", last_len)
+
+    def receive(self):
+        if len(self._ir_pulsein) == 0:
+            return
+
+        while len(self._ir_pulsein) > 0:
+            packet = self._decoder.decode(self._ir_pulsein.popleft())
+            if packet is not None:
+                return packet
+
+    def _encode_byte(self, durations, duration_index, value):
+        for i in range(8):
+            if (value & 0x80) > 0:
+                durations[duration_index] = IR_ONE
+            else:
+                durations[duration_index] = IR_ZERO
+            value <<= 1
+            duration_index += 1
+
+
+class IRDecoder(object):
+    def __init__(self):
+        self._reset_decode()
+
+    def decode(self, pulse):
+        # print("Pulse: ", pulse)
+
+        # discard pulses until we get the first header pulse
+        if self._received_headers == 0:
+            if self._check_pulse(pulse, IR_HEADER_MARK):
+                self._received_headers = 1
+        elif self._received_headers == 1:
+            if self._check_pulse(pulse, IR_HEADER_SPACE):
+                self._received_headers = 2
+                self._reset_data()
+            else:
+                self._reset_decode()
+        elif self._received_headers == 2:
+            if self._check_pulse(pulse, IR_ONE):
+                self._write_bit(1)
+            elif self._check_pulse(pulse, IR_ZERO):
+                self._write_bit(0)
+            elif self._check_pulse(pulse, IR_LEAD_OUT):
+                received_crc = self._received_data[-1]
+                received_data = self._received_data[: len(self._received_data) - 1]
+                calculated_crc = _calculate_crc(received_data)
+                pulse_error_margin = self._max_error_margin
+                self._reset_decode()
+                if received_crc == calculated_crc:
+                    error_ratio = pulse_error_margin / IR_ERROR_MARGIN
+                    signal_strength = min(1, 1.3 - error_ratio)
+                    return received_data, signal_strength
+                else:
+                    print("CRC mismatch: ", bin(received_crc), bin(calculated_crc))
+            else:
+                # unknown pulse, packet is corrupt so reset
+                self._reset_decode()
+
+    def _reset_decode(self):
+        self._received_headers = 0
+        self._max_error_margin = 0
+        self._received_data = None
+
+    def _reset_data(self):
+        self._received_data = bytearray()
+        self._reset_bits()
+
+    def _reset_bits(self):
+        self._received_byte = 0
+        self._received_bit_index = 7
+
+    def _check_pulse(self, received, expected):
+        margin = abs(received - expected)
+        match = margin < IR_ERROR_MARGIN
+        if match:
+            self._max_error_margin = max(self._max_error_margin, margin)
+        return match
+
+    def _write_bit(self, bit):
+        if bit == 1:
+            self._received_byte |= 1 << self._received_bit_index
+        self._received_bit_index -= 1
+        if self._received_bit_index < 0:
+            # print("Received Byte: ", bin(self._received_byte))
+            self._received_data.append(self._received_byte)
+            self._reset_bits()
+        # print("Bit Update: ", bin(self._received_byte), self._received_bit_index)
