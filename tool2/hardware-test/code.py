89ed25099b8037c8490a9826b75785edb99e1d0a
aaronsilinskas <aaron@mindwidgets.com>
2023-06-12 23:57:49 -0400

    move hardware init into separate file

--- tool2/hardware-test/code.py
+++ tool2/hardware-test/code.py
@@ -1,73 +1,9 @@
 import time
-from digitalio import DigitalInOut, Direction, Pull
-import pulseio
-import busio
-import board
-import neopixel
-import adafruit_rfm69
-import adafruit_lis3dh
-import adafruit_drv2605
-from mindwidgets_df1201s import DF1201S
+import hardware
 from infrared import Infrared
+import adafruit_drv2605
 
-# Pins
-PIN_NEOPIXEL = board.D5
-PIN_NEOPIXEL_5X5 = board.D10
-PIN_DFPLAYER_TX = board.TX
-PIN_DFPLAYER_RX = board.RX
-PIN_TRIGGER = board.A1
-PIN_IR_RECEIVER = board.A2
-PIN_IR_TRANSMITTER = board.A3
-
-# Set up Neopixel
-NUM_PIXELS = 12  # NeoPixel strip length (in pixels)
-strip = neopixel.NeoPixel(PIN_NEOPIXEL, NUM_PIXELS,
-                          brightness=0.25, auto_write=False)
-hud = neopixel.NeoPixel(PIN_NEOPIXEL_5X5, 5*5,
-                          brightness=0.1, auto_write=False)
-
-# Set up Radio
-RADIO_FREQ_MHZ = 915.0
-radio_chip_select = DigitalInOut(board.RFM_CS)
-radio_reset = DigitalInOut(board.RFM_RST)
-
-# Initialise RFM69 radio
-radio = adafruit_rfm69.RFM69(
-    board.SPI(), radio_chip_select, radio_reset, RADIO_FREQ_MHZ, high_power=False)
-radio.listen()
-
-# Set up I2C
-i2c = board.STEMMA_I2C()
-
-# Set up accelerometer on I2C bus, 4G range
-accel = adafruit_lis3dh.LIS3DH_I2C(i2c)
-accel.range = adafruit_lis3dh.RANGE_4_G
-accel.set_tap(1, 100)
-
-# Set up vibrator driver
-vibrator = adafruit_drv2605.DRV2605(i2c)
-
-# # Set up audio
-uart = busio.UART(tx=PIN_DFPLAYER_TX, rx=PIN_DFPLAYER_RX, baudrate=115200)
-
-df_player = DF1201S(uart)
-df_player.volume = 0.3
-df_player.disable_led()
-df_player.disable_prompt()
-df_player.enable_amp()
-df_player.play_mode = DF1201S.PLAYMODE_PLAY_ONCE
-
-# # Set up IR
-ir_pulsein = pulseio.PulseIn(pin=PIN_IR_RECEIVER, maxlen=256, idle_state=True)
-ir_pulseout = pulseio.PulseOut(
-    pin=PIN_IR_TRANSMITTER, frequency=38000, duty_cycle=2 ** 15)
-infrared = Infrared(ir_pulseout, ir_pulsein)
-
-# Set up trigger
-trigger = DigitalInOut(PIN_TRIGGER)
-trigger.direction = Direction.INPUT
-trigger.pull = Pull.UP
-
+infrared = Infrared(hardware.ir_pulseout, hardware.ir_pulsein)
 
 ###########
 
@@ -76,48 +12,47 @@ pixel_color = (64, 0, 0)
 hud_y = 0
 
 while True:
-    if trigger.value == 0:
+    if hardware.trigger.value == 0:
         print("Trigger")
         pulses = b"Hello!"
         infrared.send(pulses)
-        df_player.play_next()
-        vibrator.sequence[0] = adafruit_drv2605.Effect(83)
-        vibrator.sequence[1] = adafruit_drv2605.Effect(37)
-        vibrator.sequence[2] = adafruit_drv2605.Effect(74)
-        vibrator.play()
+        hardware.df_player.play_next()
+        hardware.vibrator.sequence[0] = adafruit_drv2605.Effect(83)
+        hardware.vibrator.sequence[1] = adafruit_drv2605.Effect(37)
+        hardware.vibrator.sequence[2] = adafruit_drv2605.Effect(74)
+        hardware.vibrator.play()
 
     ir_received = infrared.receive()
     if ir_received is not None:
         data, strength = ir_received
         print("IR Data Received: ", data.decode("utf-8"), strength)
 
-    if radio.payload_ready():
-        radio_received = radio.receive(keep_listening=True)
+    if hardware.radio.payload_ready():
+        radio_received = hardware.radio.receive(keep_listening=True)
         print("Radio Data Received: ",
-              radio_received.decode("utf-8"), radio.last_rssi)
+              radio_received.decode("utf-8"), hardware.radio.last_rssi)
 
-    strip[pixel_num] = pixel_color
+    hardware.barrel_pixels[pixel_num] = pixel_color
     pixel_num = pixel_num + 1
-    if pixel_num >= NUM_PIXELS:
+    if pixel_num >= hardware.barrel_pixels.n:
         pixel_num = 0
         if pixel_color == 0:
             pixel_color = (64, 0, 0)
         else:
             pixel_color = 0
-    strip.show()
+    hardware.barrel_pixels.show()
 
-    hud.fill(0)
-    hud_pixel = hud_y * 5
-    for p in range(hud_pixel, hud_pixel + 5):
-        hud[p] = (0,0,255)
-    hud.show()
+    hardware.hud_framebuf.fill(0)
+    hardware.hud_framebuf.line(0, hud_y, hardware.hud_framebuf.width, hud_y, (0, 0, 255))
+    hardware.hud_framebuf.display()
+    hardware.hud_pixels.show()
     hud_y += 1
-    if hud_y > 4:
+    if hud_y >= hardware.hud_framebuf.height:
         hud_y = 0
 
-    x, y, z = accel.acceleration
-    print(x, y, z)
-    if accel.tapped:
+    x, y, z = hardware.imu.acceleration
+    # print(x, y, z)
+    if hardware.imu.tapped:
         print("Tapped!")
 
     time.sleep(0.2)
---
+++ tool2/hardware-test/hardware.py
@@ -0,0 +1,77 @@
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import pulseio
+import busio
+import neopixel
+from adafruit_pixel_framebuf import PixelFramebuffer, HORIZONTAL
+import adafruit_rfm69
+import adafruit_lis3dh
+import adafruit_drv2605
+from mindwidgets_df1201s import DF1201S
+
+# Device Configuration
+
+def log(message: str):
+    print(message)
+    
+log("Device Configuration")
+
+stemma_i2c = board.STEMMA_I2C()
+
+log("..Trigger")
+trigger = DigitalInOut(board.A1)
+trigger.direction = Direction.INPUT
+trigger.pull = Pull.UP
+
+## HUD
+log("..HUD")
+hud_pixels = neopixel.NeoPixel(board.D10, 5*5,
+                          brightness=0.1, auto_write=False)
+hud_framebuf = PixelFramebuffer(
+    hud_pixels,
+    5,
+    5,
+    orientation=HORIZONTAL,
+    rotation=1,
+    alternating=False
+)
+
+hud_button_a = DigitalInOut(board.D24)
+hud_button_a.direction = Direction.INPUT
+hud_button_a.pull = Pull.UP
+
+hud_button_b = DigitalInOut(board.D25)
+hud_button_b.direction = Direction.INPUT
+hud_button_b.pull = Pull.UP
+
+log("..Barrel")
+barrel_pixels = neopixel.NeoPixel(board.D5, 12,
+                          brightness=0.25, auto_write=False)
+
+log("..Radio")
+radio_chip_select = DigitalInOut(board.RFM_CS)
+radio_reset = DigitalInOut(board.RFM_RST)
+radio = adafruit_rfm69.RFM69(
+    board.SPI(), radio_chip_select, radio_reset, frequency=915.0, high_power=False)
+radio.listen()
+
+log("..Accelerometer / IMU")
+imu = adafruit_lis3dh.LIS3DH_I2C(stemma_i2c)
+imu.range = adafruit_lis3dh.RANGE_4_G
+imu.set_tap(1, 100)
+
+log("..Vibrator / Haptic")
+vibrator = adafruit_drv2605.DRV2605(stemma_i2c)
+
+log("..Infrared")
+ir_pulsein = pulseio.PulseIn(pin=board.A2, maxlen=256, idle_state=True)
+ir_pulseout = pulseio.PulseOut(
+    pin=board.A3, frequency=38000, duty_cycle=2 ** 15)
+
+log("..DFPlayer Pro")
+df_player = DF1201S(busio.UART(tx=board.TX, rx=board.RX, baudrate=115200))
+df_player.volume = 0.1
+df_player.disable_led()
+df_player.disable_prompt()
+df_player.enable_amp()
+df_player.play_mode = DF1201S.PLAYMODE_PLAY_ONCE
