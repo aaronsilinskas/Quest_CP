107973644db2dbe394072d985e2490f0c8c4ca09
aaronsilinskas <aaron@mindwidgets.com>
2023-08-04 23:41:26 -0400

    extract turret code to Thing to get states going

--- spygame/defense/code.py
+++ spygame/defense/code.py
@@ -2,24 +2,16 @@
 SpyGame - Base Defense Turret
 """
 
-import time
 import board
 import touchio
 
 from adafruit_debouncer import Debouncer
 import neopixel
 
-from state import Thing, State
-from button_knob_input import ButtonKnobInput
-from spotlight_pixels import SpotlightPixels
+from turret_thing import TurretThing
 
 # Design
 
-# - Turret Position
-#     - [ ]  ButtonKnobInput - two buttons control rotation, variable speed and acceleration, clamped from 0 → 1
-#     - [ ]  CylonPixels - a range of pixels that gradients on both ends, optional wrapping. Background color of black.
-# - Turret Thing
-#     - [ ]  Ready → Shooting → Reloading
 # - Enemy Transport Thing
 #     - [ ]  Spawn (speed, distance, size, hp) → Moving → (Damaged → Destroyed) || Landed
 # - Enemy Fighter Thing
@@ -48,49 +40,22 @@ turn_left_button = Debouncer(lambda: turn_left_in.value, interval=0.01)
 trigger_in = touchio.TouchIn(board.A3)
 trigger_button = Debouncer(lambda: trigger_in.value, interval=0.01)
 
+# THINGS
 
-# INPUTS
-turret_position = ButtonKnobInput(left=lambda: turn_left_button.value,
-                                  right=lambda: turn_right_button.value, max_speed=0.25, acceleration=0.10, deceleration=0.15)
-# turret_position.on_move.add(lambda event: print("Turret Moved", event))
-
-# OUTPUTS
-turret_reticule = SpotlightPixels(
-    pixels, color=(0, 255, 0), width=3, wrap=True)
-
-def update_target_reticule(position: float) -> None:
-    turret_reticule.position = position
-
-turret_position.on_move.add(lambda event: update_target_reticule(event[1]))
+turret = TurretThing(pixels,
+                     turn_left_button=turn_left_button,
+                     turn_right_button=turn_right_button,
+                     trigger_button=trigger_button)
 
 # MAIN LOOP
 
-last_position: float = 0
-last_time = time.monotonic()
-
 while True:
-    current_time = time.monotonic()
-    ellapsed_time = current_time - last_time
-    last_time = current_time
-
     turn_right_button.update()
     turn_left_button.update()
     trigger_button.update()
 
-    turret_position.update(ellapsed_time)
-
     pixels.fill(0)
-    turret_reticule.show()
+
+    turret.update()
 
     pixels.show()
-
-    if turn_right_button.rose:
-        print("Turn Right Start")
-    if turn_right_button.fell:
-        print("Turn Right Stop")
-    if turn_left_button.rose:
-        print("Turn Left Start")
-    if turn_left_button.fell:
-        print("Turn Left Stop")
-    if trigger_button.rose:
-        print("Pew Pew")
---
+++ spygame/defense/turret_thing.py
@@ -0,0 +1,83 @@
+from state import Thing, State
+
+from adafruit_pixelbuf import PixelBuf
+from adafruit_debouncer import Debouncer
+
+from button_knob_input import ButtonKnobInput
+from spotlight_pixels import SpotlightPixels
+
+
+class TurretThing(Thing):
+    def __init__(self, pixels: PixelBuf, turn_left_button: Debouncer, turn_right_button: Debouncer, trigger_button: Debouncer, logging=False):
+        super().__init__(logging)
+
+        self.position_input = ButtonKnobInput(left=lambda: turn_left_button.value,
+                                              right=lambda: turn_right_button.value, max_speed=0.25, acceleration=0.10, deceleration=0.15)
+        # self.position_input.on_move.add(lambda event: print("Turret Moved", event))
+        self.trigger_button = trigger_button
+
+        self.turret_reticule = SpotlightPixels(
+            pixels, color=(0, 255, 0), width=2, wrap=True)
+
+        self.go_to_state(TurretStates.ready)
+
+    def update_inputs(self) -> None:
+        self.position_input.update(self.time_ellapsed)
+
+
+class TurretStates:
+    ready: State
+    firing: State
+    reloading: State
+    disabled: State
+
+
+class TurretReadyState(State):
+    def enter(self, thing: TurretThing):
+        thing.turret_reticule.color = (0, 255, 0)
+
+    def update(self, thing: TurretThing) -> State:
+        if thing.trigger_button.value:
+            return TurretStates.firing
+
+        thing.update_inputs()
+
+        thing.turret_reticule.position = thing.position_input.value
+        thing.turret_reticule.show()
+
+        return self
+
+
+TurretStates.ready = TurretReadyState()
+
+
+class TurretFiringState(State):
+    def enter(self, thing: TurretThing):
+        print("Pew Pew")
+        thing.turret_reticule.color = (255, 255, 255)
+
+    def update(self, thing: TurretThing) -> State:
+        thing.turret_reticule.show()
+
+        if thing.time_active > 1:
+            return TurretStates.reloading
+        return self
+
+
+TurretStates.firing = TurretFiringState()
+
+
+class TurretReloadingState(State):
+    def enter(self, thing: TurretThing):
+        print("Reloading")
+        thing.turret_reticule.color = (0, 0, 255)
+
+    def update(self, thing: Thing) -> State:
+        thing.turret_reticule.show()
+
+        if thing.time_active > 2:
+            return TurretStates.ready
+        return self
+
+
+TurretStates.reloading = TurretReloadingState()
