35d8f6d71621c7ddb5e60698a01f198b1c0cc6bd
aaronsilinskas <aaron@mindwidgets.com>
2023-08-04 23:40:32 -0400

    button_knob expose values and make time ellapsed param consistent

--- spygame/defense/button_knob_input.py
+++ spygame/defense/button_knob_input.py
@@ -14,32 +14,41 @@ class ButtonKnobInput:
 
         self._speed = 0.0
         self._value: float = 0.0
+        self._last_value = self._value
 
+    @property
+    def value(self) -> float:
+        return self._value
+    
+    @property
+    def last_value(self) -> float:
+        return self._last_value
+    
     @property
     def on_move(self) -> EventListeners:
         return self._on_move
 
-    def update(self, ellapsed_time: float) -> None:
+    def update(self, time_ellapsed: float) -> None:
         left_down = self._left()
         right_down = self._right()
         if left_down and not right_down:
-            self._speed -= self._acceleration * ellapsed_time
+            self._speed -= self._acceleration * time_ellapsed
             self._speed = max(self._speed, -self._max_speed)
         elif right_down and not left_down:
-            self._speed += self._acceleration * ellapsed_time
+            self._speed += self._acceleration * time_ellapsed
             self._speed = min(self._speed, self._max_speed)
         elif self._speed > 0:
-            self._speed -= self._deceleration * ellapsed_time
+            self._speed -= self._deceleration * time_ellapsed
             self._speed = max(self._speed, 0)
         elif self._speed < 0:
-            self._speed += self._deceleration * ellapsed_time
+            self._speed += self._deceleration * time_ellapsed
             self._speed = min(self._speed, 0)
 
-        last_value = self._value
-        self._value = self._value + self._speed * ellapsed_time
+        self._last_value = self._value
+        self._value = self._value + self._speed * time_ellapsed
         while self._value < 0:
             self._value += 1
         while self._value > 1:
             self._value -= 1
-        if self._value != last_value:
-            self._on_move.notify((last_value, self._value))
+        if self._value != self._last_value:
+            self._on_move.notify((self._last_value, self._value))
