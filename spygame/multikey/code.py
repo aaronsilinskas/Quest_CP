abc6d226d3888d9615954cb66f5d8939282e1c7c
aaronsilinskas <aaron@mindwidgets.com>
2024-07-28 23:31:25 +0200

    port multikey example code to new State of Things lib

--- spygame/multikey/code.py
+++ spygame/multikey/code.py
@@ -1,5 +1,3 @@
-#
-
 """
 Multi-Key
 Search Mode (switch off) - lights up more LEDs as signal from other keys becomes stronger
@@ -18,7 +16,7 @@ from adafruit_led_animation.animation.comet import Comet
 from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
 from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 
-from state import StateContext, State, StateMachine
+from state_of_things import State, Thing, ThingObserver
 
 # CONFIGURATION
 
@@ -34,19 +32,31 @@ CYAN = 0x00FFFF
 
 COLORS = [WHITE, RED, YELLOW, GREEN, BLUE, PURPLE, CYAN]
 
-scanAnimation = Comet(cpb.pixels, speed=0.01,
-                      color=0x333333, tail_length=5, bounce=True)
-unlockSelectAnimation = Comet(cpb.pixels, speed=0.05,
-                              color=RED, tail_length=7, bounce=False)
-unlockFailedAnimation = Comet(cpb.pixels, speed=0.02,
-                              color=YELLOW, tail_length=9, bounce=False)
+scanAnimation = Comet(
+    cpb.pixels, speed=0.01, color=0x333333, tail_length=5, bounce=True
+)
+unlockSelectAnimation = Comet(
+    cpb.pixels, speed=0.05, color=RED, tail_length=7, bounce=False
+)
+unlockFailedAnimation = Comet(
+    cpb.pixels, speed=0.02, color=YELLOW, tail_length=9, bounce=False
+)
 unlockStartingAnimation = RainbowComet(
-    cpb.pixels, speed=0.1, tail_length=7, bounce=False)
-unlockedAnimation = RainbowSparkle(
-    cpb.pixels, speed=0.1, period=5, num_sparkles=8)
+    cpb.pixels, speed=0.1, tail_length=7, bounce=False
+)
+unlockedAnimation = RainbowSparkle(cpb.pixels, speed=0.1, period=5, num_sparkles=8)
 
-unlock_order = [(1, BLUE), (3, PURPLE), (2, GREEN), (1, YELLOW),
-                (2, WHITE), (1, CYAN), (3, RED), (2, WHITE), (3, BLUE)]
+unlock_order = [
+    (1, BLUE),
+    (3, PURPLE),
+    (2, GREEN),
+    (1, YELLOW),
+    (2, WHITE),
+    (1, CYAN),
+    (3, RED),
+    (2, WHITE),
+    (3, BLUE),
+]
 
 # HARDWARE INITIALIZATION
 ble = BLERadio()
@@ -61,9 +71,20 @@ cpb.pixels.fill(0)
 # STATE MACHINE
 
 
-class MultiKeyContext(StateContext):
+class MultiKeyStates:
+    search: State
+    transmit: State
+    unlock_wait: State
+    unlock_select: State
+    unlocking: State
+    unlocked_starting: State
+    unlocked: State
+    unlock_failed: State
+
+
+class MultiKeyThing(Thing):
     def __init__(self):
-        super().__init__()
+        super().__init__(MultiKeyStates.search)
 
         # global
         self.previous_brightness = 0
@@ -79,44 +100,33 @@ class MultiKeyContext(StateContext):
         self.selected_lock = 0
         self.current_order_index = 0
         self.current_lock = 0
-        self.correct_button = ''
-
-
-class States:
-    search: State
-    transmit: State
-    unlock_wait: State
-    unlock_select: State
-    unlocking: State
-    unlocked_starting: State
-    unlocked: State
-    unlock_failed: State
+        self.correct_button = ""
 
 
 class Search(State):
-    def enter(self, context: MultiKeyContext):
-        context.closest_address = None
-        context.closest_last_time = 0
-        context.closest_rssi = -100
+    def enter(self, thing: MultiKeyThing):
+        thing.closest_address = None
+        thing.closest_last_time = 0
+        thing.closest_rssi = -100
 
-    def update(self, context: MultiKeyContext):
+    def update(self, thing: MultiKeyThing):
         if cpb.switch:
-            return States.transmit
+            return MultiKeyStates.transmit
 
         for entry in ble.start_scan(AdafruitColor, minimum_rssi=-100, timeout=1):
             if cpb.switch:
                 break
 
             now = time.monotonic()
-            if entry.address == context.closest_address:
+            if entry.address == thing.closest_address:
                 pass
-            elif entry.rssi > context.closest_rssi or now - context.closest_last_time > 0.4:
-                context.closest_address = entry.address
+            elif entry.rssi > thing.closest_rssi or now - thing.closest_last_time > 0.4:
+                thing.closest_address = entry.address
             else:
                 continue
 
-            context.closest_rssi = entry.rssi
-            context.closest_last_time = now
+            thing.closest_rssi = entry.rssi
+            thing.closest_last_time = now
             discrete_strength = min((100 + entry.rssi) // 5, 10)
             cpb.pixels.fill(BLACK)
             for i in range(0, discrete_strength):
@@ -124,7 +134,7 @@ class Search(State):
             cpb.pixels.show()
 
         now = time.monotonic()
-        if now - context.closest_last_time > 1:
+        if now - thing.closest_last_time > 1:
             cpb.pixels.fill(BLACK)
             scanAnimation.animate()
             cpb.pixels.show()
@@ -133,19 +143,19 @@ class Search(State):
         return self
 
 
-States.search = Search()
+MultiKeyStates.search = Search()
 
 
 class Transmit(State):
-    def enter(self, context: MultiKeyContext):
+    def enter(self, thing: MultiKeyThing):
         cpb.pixels.fill(0)
         cpb.pixels.show()
 
-    def update(self, context: MultiKeyContext):
+    def update(self, thing: MultiKeyThing):
         if not cpb.switch:
-            return States.search
+            return MultiKeyStates.search
         if cpb.button_a and cpb.button_b:
-            return States.unlock_wait
+            return MultiKeyStates.unlock_wait
 
         ble.start_advertising(advertisement)
         time.sleep(0.5)
@@ -154,47 +164,47 @@ class Transmit(State):
         return self
 
 
-States.transmit = Transmit()
+MultiKeyStates.transmit = Transmit()
 
 
 class UnlockWait(State):
-    def enter(self, context: MultiKeyContext):
+    def enter(self, thing: MultiKeyThing):
         pass
 
-    def update(self, context: MultiKeyContext):
+    def update(self, thing: MultiKeyThing):
         if not cpb.button_a and not cpb.button_b:
-            return States.unlock_select
+            return MultiKeyStates.unlock_select
         return self
 
 
-States.unlock_wait = UnlockWait()
+MultiKeyStates.unlock_wait = UnlockWait()
 
 
 class UnlockSelect(State):
-    def enter(self, context: MultiKeyContext):
-        context.time_held = 0
-        context.last_selected_lock = 0
+    def enter(self, thing: MultiKeyThing):
+        thing.time_held = 0
+        thing.last_selected_lock = 0
 
-    def update(self, context: MultiKeyContext):
+    def update(self, thing: MultiKeyThing):
         if cpb.button_a or cpb.button_b:
             selected_lock = 0
-            if (cpb.button_a):
+            if cpb.button_a:
                 selected_lock |= 1
-            if (cpb.button_b):
+            if cpb.button_b:
                 selected_lock |= 2
 
-            if context.last_selected_lock != selected_lock:
-                context.last_selected_lock = selected_lock
-                context.time_held = 0
+            if thing.last_selected_lock != selected_lock:
+                thing.last_selected_lock = selected_lock
+                thing.time_held = 0
                 return self
 
-            context.time_held += context.time_ellapsed
-            percent_done = context.time_held / 2
+            thing.time_held += thing.time_ellapsed
+            percent_done = thing.time_held / 2
             if percent_done > 1:
-                context.selected_lock = selected_lock
+                thing.selected_lock = selected_lock
                 while cpb.button_a or cpb.button_b:
                     True
-                return States.unlocking
+                return MultiKeyStates.unlocking
 
             pixels_to_light = int(percent_done * cpb.pixels.n)
             cpb.pixels.fill(BLACK)
@@ -202,95 +212,101 @@ class UnlockSelect(State):
                 cpb.pixels[i] = GREEN
             cpb.pixels.show()
         else:
-            context.time_held = 0
+            thing.time_held = 0
             unlockSelectAnimation.animate()
             cpb.pixels.show()
 
         return self
 
 
-States.unlock_select = UnlockSelect()
+MultiKeyStates.unlock_select = UnlockSelect()
 
 
 class Unlocking(State):
-    def enter(self, context: MultiKeyContext):
-        context.current_order_index = 0
-        context.current_lock = 0
+    def enter(self, thing: MultiKeyThing):
+        thing.current_order_index = 0
+        thing.current_lock = 0
 
-        print("Selected Lock: ", context.selected_lock)
+        print("Selected Lock: ", thing.selected_lock)
 
         cpb.pixels.fill(BLACK)
         cpb.pixels.show()
 
-    def update(self, context: MultiKeyContext):
-        active_lock, active_color = unlock_order[context.current_order_index]
-        if (context.current_lock != active_lock):
-            context.current_lock = active_lock
-            if active_lock == context.selected_lock:
+    def update(self, thing: MultiKeyThing):
+        active_lock, active_color = unlock_order[thing.current_order_index]
+        if thing.current_lock != active_lock:
+            thing.current_lock = active_lock
+            if active_lock == thing.selected_lock:
                 cpb.pixels.fill(active_color)
                 cpb.pixels.show()
             else:
-                context.correct_button = random.choice(['a', 'b'])
+                thing.correct_button = random.choice(["a", "b"])
                 other_color = random.choice(COLORS)
                 while other_color == active_color:
                     other_color = random.choice(COLORS)
                 half_of_pixels = int(cpb.pixels.n / 2)
                 for i in range(0, half_of_pixels):
-                    cpb.pixels[i] = active_color if context.correct_button == 'a' else other_color
+                    cpb.pixels[i] = (
+                        active_color if thing.correct_button == "a" else other_color
+                    )
                 for i in range(half_of_pixels, cpb.pixels.n):
-                    cpb.pixels[i] = active_color if context.correct_button == 'b' else other_color
+                    cpb.pixels[i] = (
+                        active_color if thing.correct_button == "b" else other_color
+                    )
                 cpb.pixels.show()
 
         if cpb.button_a or cpb.button_b:
-            selected_button = 'a' if cpb.button_a else 'b'
+            selected_button = "a" if cpb.button_a else "b"
             while cpb.button_a or cpb.button_b:
                 True
-            if active_lock == context.selected_lock or selected_button == context.correct_button:
-                context.current_order_index = context.current_order_index + 1
-                if context.current_order_index >= len(unlock_order):
-                    return States.unlocked_starting
+            if (
+                active_lock == thing.selected_lock
+                or selected_button == thing.correct_button
+            ):
+                thing.current_order_index = thing.current_order_index + 1
+                if thing.current_order_index >= len(unlock_order):
+                    return MultiKeyStates.unlocked_starting
             else:
-                return States.unlock_failed
+                return MultiKeyStates.unlock_failed
 
         return self
 
 
-States.unlocking = Unlocking()
+MultiKeyStates.unlocking = Unlocking()
 
 
 class UnlockedStarting(State):
-    def enter(self, context: MultiKeyContext):
-        context.previous_brightness = cpb.pixels.brightness
+    def enter(self, thing: MultiKeyThing):
+        thing.previous_brightness = cpb.pixels.brightness
 
         cpb.pixels.fill(0)
         cpb.pixels.show()
 
-    def update(self, context: MultiKeyContext):
-        if context.time_active > 10:
-            cpb.pixels.brightness = context.previous_brightness
-            return States.unlocked
+    def update(self, thing: MultiKeyThing):
+        if thing.time_active > 10:
+            cpb.pixels.brightness = thing.previous_brightness
+            return MultiKeyStates.unlocked
 
-        cpb.pixels.brightness = context.time_active / 10
-        unlockStartingAnimation.speed = max(
-            0.01, 0.5 / (1 + int(context.time_active)))
+        cpb.pixels.brightness = thing.time_active / 10
+        unlockStartingAnimation.speed = max(0.01, 0.5 / (1 + int(thing.time_active)))
         unlockStartingAnimation.animate()
         cpb.pixels.show()
 
         return self
 
 
-States.unlocked_starting = UnlockedStarting()
+MultiKeyStates.unlocked_starting = UnlockedStarting()
 
 
 class Unlocked(State):
-    def enter(self, context: MultiKeyContext):
-        context.previous_brightness = cpb.pixels.brightness
+    def enter(self, thing: MultiKeyThing):
+        thing.previous_brightness = cpb.pixels.brightness
         cpb.pixels.brightness = 0.05
 
-    def update(self, context: MultiKeyContext):
+    def update(self, thing: MultiKeyThing):
         if not cpb.switch:
-            cpb.pixels.brightness = context.previous_brightness
-            return States.search
+            cpb.pixels.brightness = thing.previous_brightness
+            return MultiKeyStates.search
 
         unlockedAnimation.animate()
         cpb.pixels.show()
@@ -298,14 +314,14 @@ class Unlocked(State):
         return self
 
 
-States.unlocked = Unlocked()
+MultiKeyStates.unlocked = Unlocked()
 
 
 class UnlockFailed(State):
 
-    def update(self, context: MultiKeyContext):
-        if context.time_active > 5:
-            return States.transmit
+    def update(self, thing: MultiKeyThing):
+        if thing.time_active > 5:
+            return MultiKeyStates.transmit
 
         unlockFailedAnimation.animate()
         cpb.pixels.show()
@@ -313,16 +329,17 @@ class UnlockFailed(State):
         return self
 
 
-States.unlock_failed = UnlockFailed()
+MultiKeyStates.unlock_failed = UnlockFailed()
+
+
+class StateChangeObserver(ThingObserver):
+    def state_changed(self, old_state: State, new_state: State):
+        print(f"State changed from {old_state.name} to {new_state.name}")
+
 
 # MAIN LOOP
-state_context = MultiKeyContext()
-state_machine = StateMachine()
-
-if cpb.switch:
-    state_machine.go_to_state(States.transmit, state_context)
-else:
-    state_machine.go_to_state(States.search, state_context)
+multi_key = MultiKeyThing()
+multi_key.observers.attach(StateChangeObserver())
 
 while True:
-    state_machine.update(state_context)
+    multi_key.update()
